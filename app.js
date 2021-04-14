71707e9b129626c63dfa080f2baeaa0a0b82b0fa
nickpaolino <nick_paolino@yahoo.com>
2021-04-14 14:03:45 -0400

    figuring out switch

--- app.js
+++ app.js
@@ -1,49 +1,78 @@
-const playerScore = 0;
-const computerScore = 0;
-const choicesArray = ['Rock', 'Paper', 'Scissors']
+
+const choicesArray = ['Rock', 'Paper', 'Scissors'];
 const message = document.getElementById('result-message');
 
 let playerChoice;
 let computerChoice;
 let result;
 
+function main() {
 
     document.getElementById('rock')
-    .addEventListener('click', (e) => {   
+    .addEventListener('click', () => {   
         playerChoice = 'Rock';
         message.textContent = `You chose ${playerChoice} :
-        Computer chose ${computerChoice}`
+        Computer chose ${computerChoice}... ${result}`;
         getComputerChoice();
+        gameResult();
     })
     document.getElementById('paper')
-    .addEventListener('click', (e) => {
+    .addEventListener('click', () => {
         playerChoice = 'Paper';
         message.textContent = `You chose ${playerChoice} : 
-        Computer chose ${computerChoice}`
+        Computer chose ${computerChoice}... ${result}`;
         getComputerChoice();
+        gameResult()
     })
     document.getElementById('scissors')
-    .addEventListener('click', (e) => {
+    .addEventListener('click', () => {
         playerChoice = 'Scissors';
         message.textContent = `You chose ${playerChoice} : 
-        Computer chose ${computerChoice}`;
+        Computer chose ${computerChoice}... ${result}`;
         getComputerChoice();
-
+        gameResult();
     })
 
-
-function getComputerChoice () {
-
-const randomChoice = Math.floor(Math.random() * 3)
-
-for (let i = 0; i < choicesArray.length; i++) {
-    i = randomChoice;
-    computerChoice = choicesArray[randomChoice];
-    break;
-  }
-
 }
 
-function gameResult () {
+function getComputerChoice() {
+    
+    const randomChoice = Math.floor(Math.random() * 3);
+    for (let i = 0; i < choicesArray.length; i++) {
+        i = randomChoice;
+        computerChoice = choicesArray[randomChoice];
+        break;
+      }
+    
+    }
+    
+function gameResult() {
 
-}
+    switch (playerChoice === "Rock") {
+        case computerChoice === "Paper":
+        result = "Sorry, you lost :(";   
+            break;
+        case computerChoice === "Scissors":
+        result = "Nice! You Win!";
+            break;
+        }
+    switch (playerChoice === "Paper") {
+        case computerChoice === "Scissors":
+        result = "Sorry, you lost :(";    
+            break;
+        case computerChoice === "Rock":
+        result = "Nice! You Win!";
+            break;
+        }
+    switch (playerChoice === "Scissors") {
+        case computerChoice === "Rock":
+        result = "Sorry, you lost :(";    
+            break;
+        case computerChoice === "Paper":
+        result = "Nice! You Win!";
+            break;
+        }
+    
+     }
+
+main()
