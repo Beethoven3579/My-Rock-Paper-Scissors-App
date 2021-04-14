c14b8a1bfbc7897752fe59d531fbe641c9a6281c
nickpaolino <nick_paolino@yahoo.com>
2021-04-13 20:46:04 -0400

    displays choices message

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 const playerScore = 0;
 const computerScore = 0;
-const choicesArray = ['rock', 'paper', 'scissors']
+const choicesArray = ['Rock', 'Paper', 'Scissors']
+const message = document.getElementById('result-message');
 
 let playerChoice;
 let computerChoice;
+let result;
 
 
-document.getElementById('rock')
-.addEventListener('click', (e) => {
-    e.preventDefault()
-    playerChoice = 'rock';
-    console.log(playerChoice)
-    getComputerChoice();
-})
-document.getElementById('paper')
-.addEventListener('click', (e) => {
-    e.preventDefault();
-    playerChoice = 'paper';
-    console.log(playerChoice)
-    getComputerChoice();
-})
-document.getElementById('scissors')
-.addEventListener('click', (e) => {
-    e.preventDefault();
-    playerChoice = 'scissors';
-    console.log(playerChoice)
-    getComputerChoice();
-})
+    document.getElementById('rock')
+    .addEventListener('click', (e) => {   
+        playerChoice = 'Rock';
+        message.textContent = `You chose ${playerChoice} :
+        Computer chose ${computerChoice}`
+        getComputerChoice();
+    })
+    document.getElementById('paper')
+    .addEventListener('click', (e) => {
+        playerChoice = 'Paper';
+        message.textContent = `You chose ${playerChoice} : 
+        Computer chose ${computerChoice}`
+        getComputerChoice();
+    })
+    document.getElementById('scissors')
+    .addEventListener('click', (e) => {
+        playerChoice = 'Scissors';
+        message.textContent = `You chose ${playerChoice} : 
+        Computer chose ${computerChoice}`;
+        getComputerChoice();
+
+    })
 
 
 function getComputerChoice () {
@@ -36,8 +39,11 @@ const randomChoice = Math.floor(Math.random() * 3)
 for (let i = 0; i < choicesArray.length; i++) {
     i = randomChoice;
     computerChoice = choicesArray[randomChoice];
-    console.log(computerChoice)
     break;
   }
+
+}
+
+function gameResult () {
 
 }
