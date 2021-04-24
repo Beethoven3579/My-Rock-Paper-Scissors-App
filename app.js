ee5c71ddf0417d507224406aeebeaec8a2c15635
nickpaolino <nick_paolino@yahoo.com>
2021-04-23 22:42:11 -0400

    refactored after review

--- app.js
+++ app.js
@@ -1,71 +1,49 @@
-
-const choicesArray = ['Rock', 'Paper', 'Scissors'];
-const message = document.getElementById('result-message');
-
-let playerChoice;
-let computerChoice;
 let result;
 
 function main() {
+    const message = document.getElementById('result-message');
+    const gameImages = document.querySelectorAll('img');
 
-    document.getElementById('rock')
-    .addEventListener('click', () => {   
-        playerChoice = 'Rock';
-        getComputerChoice();
-        gameResult();
-        message.textContent = `You chose ${playerChoice} :
-        Computer chose ${computerChoice}... ${result}`;
-    })
-    document.getElementById('paper')
-    .addEventListener('click', () => {
-        playerChoice = 'Paper';
-        getComputerChoice();
-        gameResult()
-        message.textContent = `You chose ${playerChoice} : 
-        Computer chose ${computerChoice}... ${result}`; 
-    })
-    document.getElementById('scissors')
-    .addEventListener('click', () => {
-        playerChoice = 'Scissors';
-        getComputerChoice();
-        gameResult();
-        message.textContent = `You chose ${playerChoice} : 
-        Computer chose ${computerChoice}... ${result}`;
-    })
+    for (image of gameImages) {
+        image.addEventListener('click', (e) =>  {
+            const playerChoice = capitalize(e.target.alt);
+            const computerChoice = capitalize(getComputerChoice());
+            gameResult(playerChoice, computerChoice)
+            message.textContent = `You chose ${playerChoice} :
+            Computer chose ${computerChoice}... ${result}`;
+        })
+    }  
 
 }
 
 function getComputerChoice() {
-    
-    const randomChoice = Math.floor(Math.random() * 3);
-    for (let i = 0; i < choicesArray.length; i++) {
-        i = randomChoice;
-        computerChoice = choicesArray[randomChoice];
-        break;
-    }
-    
+    const choicesArray = ['Rock', 'Paper', 'Scissors'];
+    return choicesArray[Math.floor(Math.random() * choicesArray.length)]; 
 }
     
-function gameResult() {
 
-    if (playerChoice === "Rock" && computerChoice === "Paper" || 
-    playerChoice === "Paper" && computerChoice === "Scissors" ||
-    playerChoice === "Scissors" && computerChoice === "Rock") 
-    {    result = "Sorry, you lost :-(";   
-            return;
-        }
-    if (playerChoice === "Rock" && computerChoice === "Scissors" || 
-    playerChoice === "Paper" && computerChoice === "Rock" ||
-    playerChoice === "Scissors" && computerChoice === "Paper") 
-    {    result = "Nice! You Win :-)";   
-            return;
-        }    
-    if (playerChoice === "Rock" && computerChoice === "Rock" || 
-    playerChoice === "Paper" && computerChoice === "Paper" ||
-    playerChoice === "Scissors" && computerChoice === "Scissors") 
-    {    result = "Draw";   
-            return;
-        }  
+function capitalize(string) {
+    const remainingString = string.slice(1);
+    const firstCharacter  = string.charAt(0).toUpperCase();
+    return `${firstCharacter}${remainingString}`
+}
+function gameResult(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice)  result = "Draw";
+       
+    switch(playerChoice) {
+        case 'Rock':
+            if (computerChoice === 'Scissors') result = "Nice! You Win :-)";
+            if (computerChoice === 'Paper') result = "Sorry, you lost :-("; 
+            break;
+        case 'Paper':
+            if (computerChoice === 'Rock') result = "Nice! You Win :-)";
+            if (computerChoice === 'Scissors') result = "Sorry, you lost :-(";   
+            break;
+        case 'Scissors':
+            if (computerChoice === 'Paper') result = "Nice! You Win :-)";
+            if (computerChoice === 'Rock') result = "Sorry, you lost :-(";   
+            break;    
+    }
 }  
    
 main()
